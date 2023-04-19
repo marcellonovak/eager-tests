43ec98992e1b662fbb958e77be8ec857b2ca82d1
Marcello Novak <marcellonovak@live.com>
2023-04-18 19:48:14 -0700

    Got the map test working again

--- novakm/maptest.py
+++ novakm/maptest.py
@@ -1,13 +1,13 @@
-import warnings
-
 import matplotlib.pyplot as plt
 from cartopy import crs as ccrs, feature as cfeature
+import warnings
 import test
 
-#  Suppress warnings issued by Cartopy when downloading data files
-warnings.filterwarnings('ignore')
-
+dataSet = test.ds
 projPC = ccrs.PlateCarree()
+
+# Suppress warnings issued by Cartopy when downloading data files
+warnings.filterwarnings('ignore')
 
 # Map extent, .1 degrees wider/taller than our requirements
 lonW = -116.1
@@ -20,21 +20,31 @@ res = '110m'
 
 fig = plt.figure(figsize=(9, 8))
 ax = plt.subplot(1, 1, 1, projection=projPC)
-ax.set_title('AZ Test Map')
+# fig, ax = plt.subplots(figsize=(9, 8))
 
-gl = ax.gridlines(
-    draw_labels=True, linewidth=2, color='gray', alpha=0.5, linestyle='--'
+ax.set_title('AZ Test Map')                          # Setting map title
+ax.set_extent([lonW, lonE, latS, latN], crs=projPC)  # Set map extent
+ax.coastlines(resolution=res, color='black')                       # Add coastlines  (black)
+ax.add_feature(cfeature.STATES, linewidth=0.3, edgecolor='brown')  # Add state lines (brown)
+ax.add_feature(cfeature.BORDERS, linewidth=0.5, edgecolor='blue')  # Add borderlines (blue)
+ax.gridlines(
+    draw_labels=True,
+    linewidth=2,
+    color='gray',
+    alpha=0.5,
+    linestyle='--'
 )
 
-ax.set_extent([lonW, lonE, latS, latN], crs=projPC)
-ax.coastlines(resolution=res, color='black')
-ax.add_feature(cfeature.STATES, linewidth=0.3, edgecolor='brown')
-ax.add_feature(cfeature.BORDERS, linewidth=0.5, edgecolor='blue')
-dataplot = ax.contourf(test.ds.longitude,
-                       test.ds.latitude,
-                       test.ds.t2m,
-                       transform=ccrs.PlateCarree(),
-                       levels=100)
-plt.colorbar(dataplot, orientation='horizontal', shrink=0.5)
+mesh = ax.pcolormesh(
+    dataSet.longitude,             # Latitude and Longitude
+    dataSet.latitude,
+    dataSet.t2m,                   # Actual data
+    transform=ccrs.PlateCarree(),  # Transform to fit projection
+)
 
+# Add color bar
+cbar = plt.colorbar(mesh, orientation='horizontal', pad=0.05)
+cbar.set_label('Temperature (C)')
+
+# Show plot
 fig.show()
