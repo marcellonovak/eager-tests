e196e0a68baa1d93f3808bd01de272624c2ad94a
Marcello Novak <marcellonovak@live.com>
2023-05-28 15:07:04 -0700

    Making sure all files are up to date

--- jarrelsi/test.py
+++ jarrelsi/test.py
@@ -12,9 +12,8 @@ ss = ":(?:DPT|TMP|DZDT|MSTAV|CNWAT|SPFH|WIND|PRATE|SFCR|FRICV|SHTFL|LHTFL|VEG|GF
      "[U|V]GRD:[3|5|7|8|9|10][0|5]0 mb|[U|V]GRD:10 m above ground|U[S|L]WRF:surface|RH:2 m above ground|"\
      "LFTX:500-1000 mb|TCDC:entire atmospher|HGT:[surface|equilibrum level]|PRES:cloud[base|top]|" \
      "PRES:0C isotherm)"
-H.download(ss)
-myFile = H.get_localFilePath(ss)
-myFile, myFile.exists()
 
-print(wgrib2.inventory(myFile))
+H.download(ss)
+
+print(H.inventory("C:/Users/Marce/data/hrrr/20210822/subset_3a9e5077__hrrr.t22z.wrfsfcf09.grib2"))
 
